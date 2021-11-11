06ee6a9242902dde56ec331317215e2f95901141
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-11-11 15:51:27 +0000

    workingHelloServer

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 //creating app
 
 const app = express();
+//send an HTTP response when receiving HTTP GET /
+app.get("/", (req, res) => {
+  res.send("Hello, your server is up and running and");
+});
 //make the app listen on port
 
 const port = process.argv[2] || process.env.PORT || 3000;
