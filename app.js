3aa9a4de083247f4cbd489169850b39235c78b04
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-11-14 15:09:41 +0000

    q4Done!

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 
 const app = express();
 //send an HTTP response when receiving HTTP GET /
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.send("Hello, your server is up and running and may not be running and");
+  res.sendFile("index.html", { root: __dirname });
+  //send the index.html when receiving HTTP GET /
+  // res.sendFile("public/index.html", { root: __dirname });
+  // res.send("Hello, your server is up and running and may not be running and !");
 });
 //make the app listen on port
 
