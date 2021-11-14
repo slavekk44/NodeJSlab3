a1316c13a3874a7333c1baadade2e19c68e67f71
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-11-14 16:01:45 +0000

    ejs NOT WORKING

--- app.js
+++ app.js
@@ -16,3 +16,9 @@ const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index"); //no need for ejs extension
+});
