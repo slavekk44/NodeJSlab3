f51ee480ea1b171ba338c100cf39f599e6928a1d
slavekk44 <87363097+slavekk44@users.noreply.github.com>
2021-11-18 14:31:30 +0000

    ejs work

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const express = require("express");
 
 const app = express();
 //send an HTTP response when receiving HTTP GET /
-app.use(express.static("public"));
+app.use(express.static("views"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
-  //send the index.html when receiving HTTP GET /
-  // res.sendFile("public/index.html", { root: __dirname });
-  // res.send("Hello, your server is up and running and may not be running and !");
+  // res.send("hello");
+  console.log("test ");
+  res.render("index"); //no need for ejs extension
 });
+
+//route for contacts
+
 //make the app listen on port
 
 const port = process.argv[2] || process.env.PORT || 3000;
@@ -17,8 +20,3 @@ const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
 //handling static HTML and EJS templates
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index"); //no need for ejs extension
-});
